3ed8af9f398a75512f81316e07556cf11804f4b1
Harun <62307194+Harun1409@users.noreply.github.com>
2024-12-02 21:59:49 +0100

    Navigatie Fly-out menu

---
+++ stadjutters/app/menuItems/mijnBeoordelingen.tsx
@@ -0,0 +1,22 @@
+import { Image, StyleSheet, Platform, View } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.kikker}>
+      <ThemedText>Mijn beoordelingen</ThemedText>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  kikker: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
