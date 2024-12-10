3dbbf612b803b577feaf1a1d92d333efdaa96b47
lamawizard <ahmed3lgammal@gmail.com>
2024-12-10 12:41:53 +0100

    commit

---
+++ stadjutters/app/menuItems/beoordelingPlaatsen.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function HomeScreen() {
+  const [text, setText] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Voer je naam in:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Typ hier..."
+        value={text}
+        onChangeText={(value) => setText(value)} // Bijwerken van de state
+      />
+      <Text style={styles.output}>Je hebt getypt: {text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '80%',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  output: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+});
--- stadjutters/app/menuItems/mijnBeoordelingen.tsx
+++ stadjutters/app/menuItems/mijnBeoordelingen.tsx
@@ -1,12 +1,16 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text,TextInput,Button } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 
-export default function HomeScreen() {
+export default function HomeScreen({}) {
   return (
     <View style={styles.kikker}>
       <ThemedText>Mijn beoordelingen</ThemedText>
+      <Button title='beoordeling plaatsen'
+       // onPress={() => navigation.navigate('beoordelingPlaatsen')}
+      >
+      </Button>
     </View>
   );
 }
@@ -19,4 +23,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  text: {
+    fontSize: 20,
+    color: 'black',
+  },
+  
 });
